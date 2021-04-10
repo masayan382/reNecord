da7ab6275003038b9187381d2f71aabeb4c0a6e7
m-takahashi <masan3821031@gmail.com>
2021-04-10 23:58:21 +0900

    calendar selector

--- src/Calendar/CalendarBoard.jsx
+++ src/Calendar/CalendarBoard.jsx
@@ -6,16 +6,12 @@ import styles from "./Calendar.module.css";
 import { createCalendar } from "../Dayjs/calendar";
 import { signOut } from "../features/users/usersSlice"
 
-const calendar = createCalendar();
-
 const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const CalendarBoard = () => {
-    // const calendar = createCalendar();
     const dispatch = useDispatch();
     const selector = useSelector(state => state.calendar)
-    console.log('selector:', selector);
-    console.log(calendar);
+    const calendar = createCalendar(selector);
     return (
         <div className={styles.container}>
             <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
