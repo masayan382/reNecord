2dfac4d190dbeae327cdfb458938e043ea84e1b7
m-takahashi <masan3821031@gmail.com>
2021-04-11 10:29:09 +0900

    toolbar images

--- src/Calendar/Navigation.jsx
+++ src/Calendar/Navigation.jsx
@@ -3,27 +3,32 @@ import { IconButton, Toolbar, Typography, withStyles } from "@material-ui/core";
 
 import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIos from "@material-ui/icons/ArrowForwardIos";
-import logo from "../../src/img/cal.png"
+import DehazeIcon from "@material-ui/icons/Dehaze";
 
 const StyledToolbar = withStyles({
-    root: { padding: "0 0 0 50px" }
+    root: { padding: "0" }
 })(Toolbar);
+
+const StyledTypography = withStyles({
+    root: { margin: "0 30px 0 10px" }
+})(Typography);
 
 const Navigation = () => {
     return (
         <StyledToolbar>
-            <Toolbar>
-                <img src={logo} width="40" height="40" alt="" />
-                <Typography color="textSecondary" variant="h5" component="h1">
-                    カレンダー
-            </Typography>
-                <IconButton size="small">
-                    <ArrowBackIos />
-                </IconButton>
-                <IconButton size="small">
-                    <ArrowForwardIos />
-                </IconButton>
-            </Toolbar>
+            <IconButton>
+                <DehazeIcon />
+            </IconButton>
+            <img src="/images/calendar_icon.png" width="40" height="40" />
+            <StyledTypography color="textSecondary" variant="h5" component="h1">
+                カレンダー
+            </StyledTypography>
+            <IconButton size="small">
+                <ArrowBackIos />
+            </IconButton>
+            <IconButton size="small">
+                <ArrowForwardIos />
+            </IconButton>
         </StyledToolbar>
     );
 };
